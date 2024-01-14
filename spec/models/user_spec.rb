bd4874c77bc32eee998e8dda7e5bc185fe08e99c
Kenniku Tolato <kenniku_you@ymail.com>
2024-01-14 22:24:32 +0000

    Switch and init to user specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+
+    describe 'creates a new user' do
+      it 'validates the successfull creation of a new user' do
+        fake_user = User.new(
+          first_name: 'John',
+          last_name: 'Doe',
+          email: 'john_doe@person.com',
+          password: 'qwerty',
+          password_confirmation: 'qwerty'
+        )
+
+        fake_user.save!
+        expect(fake_user.valid?).to be_truthy
+      end
+    end
+
+    context 'with the same email' do
+      it 'validates that the email has been used' do
+        existing_user = User.new(
+          first_name: 'Doe',
+          last_name: 'John',
+          email: 'john_doe@person.com',
+          password: 'qwerty',
+          password_confirmation: 'qwerty'
+        )
+
+        validation_user = User.new(
+          first_name: 'Doe',
+          last_name: 'John',
+          email: 'john_doe@person.com',
+          password: 'qwerty',
+          password_confirmation: 'qwerty'
+        )
+
+        puts User.count 
+        expect(validation_user.valid?).to be_falsey
+      end
+    end
+
+  end
 end
