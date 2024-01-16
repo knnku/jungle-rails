825eee73d6db27ab6406538222a931a83b33607f
Kenniku Tolato <kenniku_you@ymail.com>
2024-01-16 06:28:33 +0000

    Init produt details spec.

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,7 @@
+describe("Feature Spec: Product Details", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000");
+  });
+
+  it("User can navigate on a product and open it's details", () => {});
+});
