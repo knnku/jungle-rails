ac19b9ad73264f2142bba91da824107ddd9404ba
Kenniku Tolato <kenniku_you@ymail.com>
2024-01-19 07:07:01 +0000

    Add spec for add-to-cart and improvements for product spec.

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -4,6 +4,10 @@ describe("Feature Spec: Add to cart", () => {
   });
 
   it("User can add an item to the cart", () => {
+    // Set to force:true as cypress keeps scrolling the page up, covering the btn with navbar
+    cy.get('form[action="/cart/add_item?product_id=2"] button').click({ force: true });
 
+    //Check if cart has number 1
+    cy.get('a[href="/cart"]').invoke('text').should('include', '1');
   });
 });
--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -4,7 +4,9 @@ describe("Feature Spec: Product Details", () => {
   });
 
   it("User can navigate on a product and open it's details", () => {
-    cy.contains("Scented Blade").click()
+    cy.get('a[href*="products/2"]').click();
+
+    //two assertions to be sure!
     cy.url().should("eq", "http://localhost:3000/products/2");
     cy.get("p").should("contain", "found mostly in savannas");
   });
